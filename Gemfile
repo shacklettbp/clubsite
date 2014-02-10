998abb2741ac21c42e72809a270e02135d10da8b
Brennan Shacklett <bpshacklett@gmail.com>
2014-02-10 06:56:33 -0800

    woo website

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'mysql2'
+gem 'sass'
+gem 'sass-rails', '~> 4.0.0'
+gem 'haml'
+gem 'haml-rails'
+gem 'bootstrap-sass'
+
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Competition::Application.config.secret_key_base = '25790ab5c660e37cb2ba1762cafc75f4100422e2297701207efa18064f0aff31e1c0c309c2d227fad662de9643e954e713a134c66de86e002be1e9635d38916f'
